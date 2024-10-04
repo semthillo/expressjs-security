f622ceb50839c47f59972151bd015dcbce1beed1
thillo <salysem90@gmail.com>
2024-10-04 10:46:09 +0200

    Fontionnalité helmet et rateLimite ajouté

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express"
+import helmet from "helmet";
+import { rateLimit } from 'express-rate-limit'
 import { router } from "./routes/index.js"
 import bodyParser from "body-parser"
 
 const app = express()
 app.use(bodyParser.json())
 app.use(router)
+app.use(helmet());
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 5,
+  });
 
 
+app.use(limiter);
 app.listen(3001, () => {
     console.log("Application connecté au port 3001");
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const message = "hello world"
 
-router.get('/', (req, res)=>{
+router.get('/api/hello', (req, res)=>{
     res.send(message)
 } )
 
